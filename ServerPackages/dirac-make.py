cfe0543b73ac678595e0b3707dc46e5d8f9d7bbe
Zoltan Mathe <zoltan.mathe@cern.ch>
2016-06-09 17:05:06 +0200

    add new modules needed for v6r16

--- ServerPackages/dirac-make.py
+++ ServerPackages/dirac-make.py
@@ -22,7 +22,9 @@ ch = chClass( here )
 versions = { 'sqlalchemy' : "1.0.9",
              'pexpect' : '4.0.1',
              'MySQL-python' : '1.2.5',
-             'requests' : '2.9.1'}
+             'requests' : '2.9.1',
+             'elasticsearch-dsl':'2.0.0',
+             'psutil':'4.2.0'}
 
 ch.setPackageVersions( versions )
 
